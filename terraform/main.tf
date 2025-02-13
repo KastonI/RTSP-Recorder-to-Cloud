89ade071ea0c3b18a01a8890a452b05b25ed4a06
KastonI <ilas70587@gmail.com>
2025-02-13 14:31:01 +0100

    Fix egress in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -97,7 +97,7 @@ resource "aws_security_group" "private_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = { Name = "Private Security Group" }
