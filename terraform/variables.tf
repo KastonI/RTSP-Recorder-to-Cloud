277f2c2cbd51ab4d263812aabdbf550ab8fb0464
Illia Savchenko <ilas70587@gmail.com>
2025-03-03 10:36:53 +0100

    Comment dafults in variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,41 +1,40 @@
 variable "aws_region" {
-  default     = "eu-central-1"
+  default     = "" #"eu-central-1"
   description = "Aws region"
 }
 
 variable "vpc_cidr" {
-  default     = "10.0.0.0/16"
+  default     = "" #"10.0.0.0/16"
   description = "CIDR block for VPC"
 }
 
 variable "public_subnet_cidr" {
-  default     = "10.0.1.0/24"
+  default     = "" #"10.0.1.0/24"
   description = "Public CIDR for VPC"
 }
 
 variable "private_subnet_cidr" {
-  default     = "10.0.2.0/24"
+  default     = "" #"10.0.2.0/24"
   description = "Private CIDR for VPC"
 }
 
-
 variable "availability_zone" {
-  default     = "eu-central-1a"
+  default     = "" #"eu-central-1a"
   description = "Availability zone"
 }
 
 variable "aws_instance" {
-  default     = "ami-07eef52105e8a2059"
+  default     = "" #"ami-07eef52105e8a2059"
   description = "AMI for EC2 instances"
 }
 
 variable "instance_type" {
-  default     = "t2.micro"
+  default     = ""  #"t2.micro"
   description = "Instance type for common EC2"
 }
 
 variable "instance_type_rtsp" {
-  default     = "t2.medium"
+  default     = ""  #"t2.medium"
   description = "Instance type for RTSP EC2"
 }
 
@@ -44,13 +43,7 @@ variable "ssh_public_key" {
   description = "Public key for instances"
 }
 
-variable "ssh_public_bastion" {
-  default     = ""
-  description = "Public key for Bastion"
-}
-
-
 variable "s3_records" {
-  default     = "s3-for-records1"
+  default     = "" #"s3-for-records1"
   description = "S3 backet for records"
 }
